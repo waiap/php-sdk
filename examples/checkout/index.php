8c3a4ebe3f720f227bc232f0e793e9a77266ffba
Miguel Ángel Curiel Domínguez <macuriel@sipay.es>
2020-04-06 17:06:56 +0200

    Update index.php

--- examples/checkout/index.php
+++ examples/checkout/index.php
@@ -4,9 +4,9 @@
     $server_request = file_get_contents('php://input');
     $client = new \PWall\Client();
     $client->setEnvironment("sandbox");
-    $client->setKey("sipay-test-team");
-    $client->setResource("sipay-test-pwall");
-    $client->setSecret("api-secret");
+    $client->setKey("<inserte su key proporcionada por su integrador>");
+    $client->setResource("<insert el nombre del resource proporcionado por su integrador");
+    $client->setSecret("<inserte el secret porporcionado por su integrador>");
     $client->setBackendUrl("https://domain.host/example/checkout.html");
     $client->setDebugFile("/var/www/log/debug.log"); // logger .   
     $request = new \PWall\Request($server_request, false);
